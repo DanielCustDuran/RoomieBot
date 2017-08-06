1ad99c8d6058fb4725daba6c0137b68bd93bdce8
DanielCustDuran <danielisidrocustodioduran@gmail.com>
2017-08-05 23:03:26 -0500

    Cambios para hacer commit: 	borrado:       ai.py 	borrado:       app.pyc 	borrado:       app1.py 	borrado:       conexion.pyc 	borrado:       config 	borrado:       config.pyc 	borrado:       connect_db.pyc 	borrado:       j.py 	borrado:       patrons.pyc 	borrado:       translator.py 	borrado:       translator.pyc

--- ai.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from translator import translator_lang_en, translator_lan_es
-
-def get_tokens(sentence):
-	tokens = nltk.word_tokenize(sentence.lower())
-	tagged = nltk.pos_tag(tokens)
-
-get_tokens(translator_lang_en("HOLA, QUIERO IR A CIUDAD DE MEXICO"))
